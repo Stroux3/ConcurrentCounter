d8274f9636e86a36233e09e580f9c9b263291b20
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-18 17:56:30 +0300

    Создание класса без использования ReaderWriterLockSlim и реализация методов начала и завершения чтения

---
+++ ConcurrentCounter.Core/Services/ServerManual.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ConcurrentCounter.Core.Services
+{
+    /// <summary>
+    /// Потокобезопасный счётчик без использования ReaderWriterLockSlim.
+    /// Поддерживает параллельное чтение и эксклюзивную запись.
+    /// </summary>
+    public static class ServerManual
+    {
+        private static int count = 0;
+
+        private static int readers = 0;
+        private static readonly Lock readerLock = new(); // защита счётчика читателей
+        private static readonly SemaphoreSlim writerSemaphore = new(1, 1); // эксклюзивная запись
+        private static void BeginRead()
+        {
+            writerSemaphore.Wait();
+            lock (readerLock)
+            {
+                readers++;
+            }
+            writerSemaphore.Release();
+        }
+        private static void EndRead()
+        {
+            lock (readerLock)
+            {
+                readers--;
+            }
+        }
+
+        
+    }
+}
