b9513ea87432f5f8d8bcb59973c13d79c14634b5
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-18 19:16:06 +0300

    Реализация и вывод результата первого условия: Читатели могли читать параллельно, не блокируя друг друга;

--- ConcurrentCounterApp/Program.cs
+++ ConcurrentCounterApp/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConcurrentCounter;
+using ConcurrentCounter.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+       Console.WriteLine("\t\t\t\tВсе читатели параллельно читают одно и тоже значение");
+       await MultipleReaders_ShouldReadSameValue_ReaderWriterLockSlim();
+       await MultipleReaders_ShouldReadSameValue_Manual();
+    }
+    #region Все читатели параллельно читают одно и тоже значение
+
+    #region Реализация через ReaderWriterLockSlim
+    public static async Task MultipleReaders_ShouldReadSameValue_ReaderWriterLockSlim()
+    {
+        Console.WriteLine($"\nРеализация через ReaderWriterLockSlim\n");
+        ServerWithReaderWriterLockSlim.Reset();
+        ServerWithReaderWriterLockSlim.AddToCount(10);
+
+        var tasks = Enumerable.Range(0, 5)
+            .Select(i => Task.Run(() => (Index: i, Value: ServerWithReaderWriterLockSlim.GetCount())))
+            .ToArray();
+
+        var results = await Task.WhenAll(tasks);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine($"Читатель {result.Index} читает значение: {result.Value}");
+        }
+    }
+    #endregion
+
+    #region Реализация без использования ReaderWriterLockSlim
+    public static async Task MultipleReaders_ShouldReadSameValue_Manual()
+    {
+        Console.WriteLine($"\nРеализация без использования ReaderWriterLockSlim\n");
+        ServerManual.Reset();
+        ServerManual.AddToCount(20);
+
+        var tasks = Enumerable.Range(0, 5)
+            .Select(i => Task.Run(() => (Index: i, Value: ServerManual.GetCount())))
+            .ToArray();
+
+        var results = await Task.WhenAll(tasks);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine($"Читатель {result.Index} читает значение: {result.Value}");
+        }
+    }
+    #endregion
+    #endregion
+
+}
+
+
