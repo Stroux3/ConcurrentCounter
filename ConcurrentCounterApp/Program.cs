92350ceaddab5630655285ba7912168d836e37e8
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-18 19:18:14 +0300

    Реализация и вывод результата второго условия: Писатели писали только последовательно и никогда одновременно;

--- ConcurrentCounterApp/Program.cs
+++ ConcurrentCounterApp/Program.cs
@@ -11,6 +11,10 @@ class Program
        Console.WriteLine("\t\t\t\tВсе читатели параллельно читают одно и тоже значение");
        await MultipleReaders_ShouldReadSameValue_ReaderWriterLockSlim();
        await MultipleReaders_ShouldReadSameValue_Manual();
+
+       Console.WriteLine("\t\t\t\tВсе писатели пишут последовательно и никогда одновременно");
+       await Writers_ShouldBeSequentialAndAccurate_ReaderWriterLockSlim();
+       await Writers_ShouldBeSequentialAndAccurate_Manual();
     }
     #region Все читатели параллельно читают одно и тоже значение
 
@@ -54,6 +58,49 @@ class Program
     }
     #endregion
     #endregion
+
+    #region Все писатели пишут последовательно и никогда одновременно
+    #region Реализация через ReaderWriterLockSlim
+    public static async Task Writers_ShouldBeSequentialAndAccurate_ReaderWriterLockSlim()
+    {
+        Console.WriteLine($"\nРеализация через ReaderWriterLockSlim\n");
+        ServerWithReaderWriterLockSlim.Reset();
+        int count = 5;
+        var tasks = Enumerable.Range(0, 10)
+            .Select(i => Task.Run(() =>
+            {
+                ServerWithReaderWriterLockSlim.AddToCount(count);
+                int current = ServerWithReaderWriterLockSlim.GetCount();
+                Console.WriteLine($"Писатель {i} пишет {count}, текущие значение: {current}");
+            }))
+            .ToArray();
+
+        await Task.WhenAll(tasks);
+    }
+    #endregion
+
+    #region Реализация без использования ReaderWriterLockSlim
+    public static async Task Writers_ShouldBeSequentialAndAccurate_Manual()
+    {
+        Console.WriteLine($"\nРеализация без использования ReaderWriterLockSlim\n");
+        ServerManual.Reset();
+        int count = 5;
+        var tasks = Enumerable.Range(0, 10)
+            .Select(i => Task.Run(() =>
+            {
+                ServerManual.AddToCount(count);
+                int current = ServerManual.GetCount();
+                Console.WriteLine($"Писатель {i} пишет {count}, текущие значение: {current}");
+            }))
+            .ToArray();
+
+        await Task.WhenAll(tasks);
+    }
+    #endregion
+
+    #endregion
+
+
 
 }
 
