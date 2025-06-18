da3d5e12b2ff5af1a0053ca550d87595dbd316a6
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-18 17:43:17 +0300

    Реализация класса Server и метода параллельного чтения

--- ConcurrentCounter.Core/Services/Server.cs
+++ ConcurrentCounter.Core/Services/Server.cs
@@ -1,6 +1,29 @@
-﻿namespace ConcurrentCounter.Core.Services;
-
-public class Server
+﻿namespace ConcurrentCounter.Core.Services
 {
+    /// <summary>
+    /// Класс, реализующий потокобезопасный счётчик с поддержкой параллельного чтения и последовательной записи.
+    /// </summary>
+    public static class Server
+    {
+        private static readonly int count = 0;
 
+        private static readonly ReaderWriterLockSlim rwLock = new();
+
+        /// <summary>
+        /// Возвращает текущее значение счётчика.
+        /// Множественные вызовы могут выполняться параллельно.
+        /// </summary>
+        public static int GetCount()
+        {
+            rwLock.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                rwLock.ExitReadLock();
+            }
+        }
+    }
 }
